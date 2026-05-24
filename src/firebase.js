ecd0b75a965955e13b867b6cc8c18c5571785d4a
Jayton Hollis <jaytonhollis@MacBook-Pro-8.local>
2026-05-24 17:42:06 -0500

    Add real Firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -20,12 +20,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "REPLACE_ME",
-  authDomain: "REPLACE_ME.firebaseapp.com",
-  projectId: "REPLACE_ME",
-  storageBucket: "REPLACE_ME.appspot.com",
-  messagingSenderId: "REPLACE_ME",
-  appId: "REPLACE_ME",
+  apiKey: "AIzaSyDitdhiNXB_NyMOkknKTsgIgJ3TOnLaQKQ",
+  authDomain: "doink-2c46c.firebaseapp.com",
+  projectId: "doink-2c46c",
+  storageBucket: "doink-2c46c.firebasestorage.app",
+  messagingSenderId: "937252559926",
+  appId: "1:937252559926:web:faa5501c1766acd74e8c97",
 };
 
 const app = initializeApp(firebaseConfig);
